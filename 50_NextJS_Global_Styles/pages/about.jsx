bb826bcfdc4b0bdb1fd3f48ed5ff8eee29e823ad
danielOuattara <daniel.ouattara@gmx.com>
2023-09-12 23:43:03 +0200

    49 next js global styles

---
+++ 50_NextJS_Global_Styles/pages/about.jsx
@@ -0,0 +1,10 @@
+export default function About() {
+  return (
+    <>
+      <h2>About Page</h2>
+
+      <button className="btn btn-primary mx-2">Click</button>
+      <button className="btn btn-success mx-2">Click</button>
+    </>
+  );
+}
