31649e318a3c6c3c48f98a565a1095b7d8203773
danielOuattara <daniel.ouattara@gmx.com>
2023-09-12 23:43:27 +0200

    50 next js component level styles

---
+++ 51_NextJS_Component_Level_Styles/pages/about.jsx
@@ -0,0 +1,12 @@
+import * as styles from "./../styles/About.module.css";
+
+export default function About() {
+  return (
+    <>
+      <h2 className={styles.heading}>About Page</h2>
+
+      <button className="btn btn-primary mx-2">Click</button>
+      <button className="btn btn-success mx-2">Click</button>
+    </>
+  );
+}
---
+++ 51_NextJS_Component_Level_Styles/pages/contact.jsx
@@ -0,0 +1,5 @@
+import * as styles from "./../styles/Contact.module.css";
+
+export default function Contact() {
+  return <h2 className={styles.heading}>Contact Page</h2>;
+}
