3861fd3e4164b2c73c0a300bb8dee0672530b138
danielOuattara <daniel.ouattara@gmx.com>
2023-11-06 21:46:55 +0100

    21 update logic +  diapos

--- 21_running_static_generation_builds/pages/index.jsx
+++ 21_running_static_generation_builds/pages/index.jsx
@@ -4,6 +4,10 @@ export default function Home() {
   return (
     <>
       <h1>Next JS pre-rendering</h1>
+      {/*  
+      Analyze the build code (npm run build & npm start).
+      Then add the Link below and analyze again to understand the difference 
+      */}
       <Link href="/users">go to users</Link>
     </>
   );
