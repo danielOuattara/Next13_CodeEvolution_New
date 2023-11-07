cc86cab04924fb238192a1e04db6c45574f28787
danielOuattara <daniel.ouattara@gmx.com>
2023-11-07 23:10:14 +0100

    24 update logic +  diapos + folder name

--- 24_inspecting_getStaticPath_in_builds/pages/posts/[postId].jsx
+++ 24_inspecting_getStaticPath_in_builds/pages/posts/[postId].jsx
@@ -1,8 +1,4 @@
-import React from "react";
-
-//-----------------
 export default function SinglePostDetails(props) {
-  // console.log("props = ", props);
   return (
     <article>
       <h2>{props.post.title}</h2>
@@ -14,8 +10,6 @@ export default function SinglePostDetails(props) {
 
 //-----------------
 export async function getStaticProps(context) {
-  // log("context = ", context);
-
   const postId = context.params.postId;
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${postId}`,
--- 24_inspecting_getStaticPath_in_builds/pages/posts/index.jsx
+++ 24_inspecting_getStaticPath_in_builds/pages/posts/index.jsx
@@ -1,4 +1,3 @@
-import { log } from "console";
 import Link from "next/link";
 
 export default function PostsHomePage(props) {
@@ -18,15 +17,13 @@ export default function PostsHomePage(props) {
 
 //--------------------------------------------------------
 
-export async function getStaticProps(context) {
-  log("context = ", context);
-
-  const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+export async function getStaticProps() {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=5`,
+  );
   const fetchedPosts = await response.json();
 
-  log(fetchedPosts.slice(0, 5));
-
   return {
-    props: { posts: fetchedPosts.slice(0, 5) },
+    props: { posts: fetchedPosts },
   };
 }
