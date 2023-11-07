05f4fb30394a4d89e61440a1d52d1d3851a21818
danielOuattara <daniel.ouattara@gmx.com>
2023-11-07 23:10:40 +0100

    26 logic updated

--- 26_getStaticPaths_and_fallback_false/pages/posts/[postId].jsx
+++ 26_getStaticPaths_and_fallback_false/pages/posts/[postId].jsx
@@ -1,8 +1,4 @@
-import React from "react";
-
-//-----------------
 export default function SinglePostDetails(props) {
-  // console.log("props = ", props);
   return (
     <article>
       <h2>{props.post.title}</h2>
@@ -14,8 +10,6 @@ export default function SinglePostDetails(props) {
 
 //-----------------
 export async function getStaticProps(context) {
-  // log("context = ", context);
-
   const postId = context.params.postId;
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${postId}`,
@@ -31,11 +25,11 @@ export async function getStaticProps(context) {
 
 //-----------------
 export async function getStaticPaths() {
-  const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
-  const posts = await response.json();
-  const postPaths = posts.map((post) => ({
-    params: { postId: `${post.id}` },
-  }));
+  // const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  // const posts = await response.json();
+  // const postPaths = posts.map((post) => ({
+  //   params: { postId: `${post.id}` },
+  // }));
 
   return {
     paths: [
--- 26_getStaticPaths_and_fallback_false/pages/posts/index.jsx
+++ 26_getStaticPaths_and_fallback_false/pages/posts/index.jsx
@@ -2,26 +2,39 @@ import Link from "next/link";
 
 export default function PostsHomePage(props) {
   return (
-    <ul>
-      {props.posts.map((post) => (
-        <article key={post.id}>
-          <Link href={`/posts/${post.id}`}>
-            <h2>{post.title}</h2>
-          </Link>
-          <hr />
-        </article>
-      ))}
-    </ul>
+    <>
+      <h1>Posts page</h1>
+      <p>
+        Comment and uncomment the "ul" in code to see the difference in Next.js
+        fetching capabilities.
+      </p>
+      <p>
+        If no "ul" and "li" html elements displayed here, then use the address
+        to navigate to a specific post
+      </p>
+      {/* <ul>
+        {props.posts.map((post) => (
+          <article key={post.id}>
+            <Link href={`/posts/${post.id}`}>
+              <h2>{post.title}</h2>
+            </Link>
+            <hr />
+          </article>
+        ))}
+      </ul> */}
+    </>
   );
 }
 
 //--------------------------------------------------------
 
 export async function getStaticProps(context) {
-  const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=5`,
+  );
   const posts = await response.json();
 
   return {
-    props: { posts: posts.slice(0, 3) },
+    props: { posts },
   };
 }
